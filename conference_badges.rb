1caa0c96775ea4fe75e3e2b1f6a74decb3c2da2d
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-19 18:59:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badged_names = []
+  array.each do |name|
+    badged_names << badge_maker(name)
+  end
+end
+    
